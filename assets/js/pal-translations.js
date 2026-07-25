03a8615d9712b7f4e7564c7cab94f75a350590e4
Pennjohn12 <pennjohn12@proton.me>
2026-07-25 01:56:06 -0400

    update

--- assets/js/pal-translations.js
+++ assets/js/pal-translations.js
@@ -1,6 +1,6 @@
-import { PAL_APP_TRANSLATIONS } from "./i18n/app-translations.js";
-import { PAL_HOME_TRANSLATIONS } from "./i18n/home-translations.js";
-import { PAL_ORIENTATION_TRANSLATIONS } from "./i18n/orientation-translations.js";
+import { PAL_APP_TRANSLATIONS } from "./i18n/app-translations.js?v=2026-07-25-weekly-toolbox";
+import { PAL_HOME_TRANSLATIONS } from "./i18n/home-translations.js?v=2026-07-25-weekly-toolbox";
+import { PAL_ORIENTATION_TRANSLATIONS } from "./i18n/orientation-translations.js?v=2026-07-25-weekly-toolbox";
 
 export const PAL_TRANSLATIONS = {
   app: PAL_APP_TRANSLATIONS,
